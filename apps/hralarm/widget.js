9ee225cdb5df2a804077bb2c012f7a39c70a9b87
QNerk <127989047+QNerk@users.noreply.github.com>
2023-04-17 20:57:25 +0200

    Update widget.js

--- apps/hralarm/widget.js
+++ apps/hralarm/widget.js
@@ -1,39 +1,42 @@
-(() => {
-  var settings = require('Storage').readJSON("hralarm.json", true) || {};
-  if (!settings.enabled){ Bangle.setHRMPower(0, 'hralarm'); return; }
-  Bangle.setHRMPower(1, 'hralarm');
-  var hitLimit = 0;
-  var checkHr = function(hr){
-      
-    NRF.setAdvertising({},{
-      showName: false,
-      manufacturer: 0x0590,
-      manufacturerData: JSON.stringify({ name: "ALARM" })
-      });
-    
-    if (hr.bpm > settings.warning && hr.bpm <= settings.upper){
-      Bangle.buzz(100, 1)
-      ;
-    }
-    if (hitLimit < getTime() && hr.bpm > settings.upper){
-      hitLimit = getTime() + 10;
-      Bangle.buzz(2000, 1);
+var hrAlertWidget = {
+  name: "HR Alert by Q_Nerk",
+  width: 24, // Width of widget in pixels
+  height: 24, // Height of widget in pixels
+  lastAlert: 0,
+  render: function(display) {
+    // Get the current heart rate from the monitor
+    var hr = E.HRM.get().raw;
 
+    // Check if the heart rate is out of range
+    if (hr < 60 || hr > 100) {
+      // Send a Bluetooth message if it has been at least 10 seconds since the last alert
+      if ((Date.now() - this.lastAlert) > 10000) {
+        var message = "Heart rate out of range: " + hr.toString();
+        NRF.requestDevice({ filters: [{ namePrefix: 'ESP32' }] })
+          .then(function(device) {
+            device.gatt.connect()
+              .then(function(server) {
+                server.getPrimaryService('6e400001-b5a3-f393-e0a9-e50e24dcca9e')
+                  .then(function(service) {
+                    service.getCharacteristic('6e400002-b5a3-f393-e0a9-e50e24dcca9e')
+                      .then(function(characteristic) {
+                        characteristic.writeValue(message);
+                        console.log("Bluetooth message sent: " + message);
+                      });
+                  });
+              });
+          });
+        this.lastAlert = Date.now();
+      }
     }
-    if (hitLimit > 0 && hr.bpm < settings.lower){
-      hitLimit = 0;
-      Bangle.buzz(500, 1);
-    }
-    if (hitLimit < getTime() && hr.bpm > settings.bottom){
-      hitLimit = getTime() + 10;
-      Bangle.buzz(2000, 1);
-  };
-  Bangle.on("HRM", checkHr);
-  Bangle.on("BTHRM", checkHr);
 
-  WIDGETS["hralarm"]={
-    area:"tl",
-    width: 0,
-    draw: function(){}
-  };
-})()
+    // Draw the heart rate value on the widget
+    display.setFontAlign(-1,-1); // Align text to top-left corner
+    display.drawString(hr.toString(), 0, 0);
+  },
+  interval: 1000 // Update interval in milliseconds
+};
+
+// Add the widget to the widget list
+Bangle.loadWidgets();
+Bangle.addWidgets([hrAlertWidget]);
